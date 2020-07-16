648e5e4c005b2148b3a1fd6a0d66c431ac34767a
divya <divyahk73@gmail.com>
2020-07-15 22:34:50 -0700

    inintial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+import {createStore} from 'redux'
+import {Provider} from "react-redux"
+import reducer from './reducer'
+
+const initialStore ={
+  cart:cartItems,
+  total:105,
+  amount:5
+}
+
+
+const store =createStore(reducer,initialStore);
+
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer  />
+    </Provider>
+  );
+}
+
+export default App;
